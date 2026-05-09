5408b439bacc17b2eccdf130224fd04f733a071b
Michelle Schmid <schmim89@students.zhaw.ch>
2026-05-09 14:10:53 +0200

    deleted self update error

--- src/game_logic/FieldCollection.py
+++ src/game_logic/FieldCollection.py
@@ -1,4 +1,3 @@
-from typing import Self
 from game_logic.Field import Field
 from game_logic.Player import Player
 
@@ -28,7 +27,7 @@ class FieldCollection:
 
         return fields
 
-    def clone(self) -> Self:
+    def clone(self):
         # Create empty object instead of using the initializer unnecessarily
         clone = object.__new__(type(self))
 
